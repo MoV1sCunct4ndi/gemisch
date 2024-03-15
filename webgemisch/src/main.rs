458f50f6e0b94b605ad0f5d979f0dfd589e0b6d3
MoV1sCunct4ndi <martin.bauernfeind99@web.de>
2024-03-15 16:14:26 +0100

    :sparkles: use yew-router for navigation
    Co-authored-by: Henny022 <git@henny022.de>

--- webgemisch/src/main.rs
+++ webgemisch/src/main.rs
@@ -1,7 +1,10 @@
-use std::collections::HashMap;
-use yew::prelude::*;
-use gemisch::{Ingredient, Recipe};
 use gemisch::amount::{Amount, MeasurementUnit};
+use gemisch::{Ingredient, Recipe};
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[derive(Eq, Hash, PartialEq, Clone)]
 struct RecipeIdentifier(String);
@@ -12,29 +15,36 @@ impl From<String> for RecipeIdentifier {
     }
 }
 
-#[derive(Default)]
-enum State {
-    #[default]
-    ListRecipes,
-    ShowRecipe(RecipeIdentifier),
+impl Display for RecipeIdentifier {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
 
-#[derive(Default)]
-struct App {
-    state: State,
-    ingredients: HashMap<String, Ingredient>,
-    recipes: HashMap<RecipeIdentifier, Recipe>,
+impl FromStr for RecipeIdentifier {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(s.to_string().into())
+    }
 }
 
-fn list_recipes(recipes: &HashMap<RecipeIdentifier, Recipe>, ctx: &Context<App>) -> Html {
-    html!{
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/recipe/:id")]
+    Recipe { id: RecipeIdentifier },
+}
+
+fn list_recipes(recipes: &HashMap<RecipeIdentifier, Recipe>) -> Html {
+    html! {
         <div>
             <h1>{"Recipes"}</h1>
             <ul>
                 { for recipes.iter().map(|(_, v)|{
                     let id: RecipeIdentifier = v.name.clone().into();
-                    let onclick = ctx.link().callback(move |_| Message::ShowRecipe(id.clone()));
-                    html!{ <li {onclick}>{&v.name}</li> }
+                    html!{ <li><Link<Route> to={Route::Recipe{id}}>{&v.name}</Link<Route>></li> }
                 })}
             </ul>
         </div>
@@ -62,48 +72,70 @@ fn show_recipe(recipe: &Recipe) -> Html {
                 })}
             </ol>
         </>
-
     }
 }
 
-enum Message {
-    ShowRecipe(RecipeIdentifier),
+#[derive(Default)]
+struct DataStore {
+    ingredients: HashMap<String, Ingredient>,
+    recipes: HashMap<RecipeIdentifier, Recipe>,
 }
 
-impl Component for App {
-    type Message = Message;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
+impl DataStore {
+    fn new() -> Self {
         let mut self_ = Self::default();
         let coke = Ingredient::new("Cola".to_owned());
         let apple_juice = Ingredient::new("Apfelsaft".to_owned());
-        let mut apple_coke = Recipe::new("Apfelcola".to_owned(), vec![
-            (&coke, Amount { value: 0.2.into(), unit: MeasurementUnit::Volume }),
-            (&apple_juice, Amount { value: 0.2.into(), unit: MeasurementUnit::Volume }),
-        ]);
-        apple_coke.instructions = vec!["Cola eingießen".to_owned(), "Apfelsaft eingießen".to_owned()];
+        let mut apple_coke = Recipe::new(
+            "Apfelcola".to_owned(),
+            vec![
+                (
+                    &coke,
+                    Amount {
+                        value: 0.2.into(),
+                        unit: MeasurementUnit::Volume,
+                    },
+                ),
+                (
+                    &apple_juice,
+                    Amount {
+                        value: 0.2.into(),
+                        unit: MeasurementUnit::Volume,
+                    },
+                ),
+            ],
+        );
+        apple_coke.instructions = vec![
+            "Cola eingießen".to_owned(),
+            "Apfelsaft eingießen".to_owned(),
+        ];
         self_.ingredients.insert(coke.name.clone(), coke);
-        self_.ingredients.insert(apple_juice.name.clone(), apple_juice);
-        self_.recipes.insert(apple_coke.name.clone().into(), apple_coke);
+        self_
+            .ingredients
+            .insert(apple_juice.name.clone(), apple_juice);
+        self_
+            .recipes
+            .insert(apple_coke.name.clone().into(), apple_coke);
         self_
     }
+}
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg { Message::ShowRecipe(recipe) => { self.state = State::ShowRecipe(recipe) } };
-        true
-    }
+#[function_component(App)]
+pub fn app() -> Html {
+    let data = use_mut_ref(|| DataStore::new());
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        match &self.state {
-            State::ListRecipes => list_recipes(&self.recipes, ctx),
-            State::ShowRecipe(recipe) => {
-                match self.recipes.get(recipe) {
-                    None => html! {<h1>{format!("Recipe '{}' not found", recipe.0)}</h1>},
-                    Some(recipe) => show_recipe(recipe),
-                }
-            },
-        }
+    let switch = move |route| match route {
+        Route::Home => list_recipes(&data.borrow().recipes),
+        Route::Recipe { id } => match data.borrow().recipes.get(&id) {
+            None => html! {<h1>{format!("Recipe '{}' not found", id.0)}</h1>},
+            Some(recipe) => show_recipe(&recipe),
+        },
+    };
+
+    html! {
+        <HashRouter>
+            <Switch<Route> render={switch} />
+        </HashRouter>
     }
 }
 
